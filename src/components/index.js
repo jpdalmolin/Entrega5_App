fec596a0e2608b4c7a2c646b33b0161af2cc92c0
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2023-02-13 19:13:46 -0300

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as AddItem} from './add-item';
+export { default as TaskList} from './task/index'
+export { default as CustomModal} from './modal/index'
+export {default as Card} from './card/index';
+export {default as Header} from './header/index';
+export {default as TextContainer} from './text-container/index';
---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+export {default as Buyscreen} from "./buyscreen/index";
+export {default as HomeScreen} from "./homescreen/index";
+export  {default as Cart} from "./cart/index";
+export  {default as Orders} from "./orders/index";
